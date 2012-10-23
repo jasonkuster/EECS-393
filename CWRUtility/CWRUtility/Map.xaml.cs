88d760244e1a04aa2224a9245e67fd8ecc494cfa
Stuart <illusivestu@gmail.com>
2012-10-23 00:03:05 -0400

    Map Shit

--- CWRUtility/CWRUtility/Map.xaml.cs
+++ CWRUtility/CWRUtility/Map.xaml.cs
@@ -12,6 +12,8 @@ namespace CWRUtility
 {
     public partial class Map : PhoneApplicationPage
     {
+        double initialScale;
+
         public Map()
         {
             InitializeComponent();
@@ -23,8 +25,19 @@ namespace CWRUtility
             actualMap.Mode = new CWRUMapMode();
 
             //actualMap.SetView(mapCenter, 16);
-            ContentPanel.Children.Add(actualMap);
+            bmapGrid.Children.Add(actualMap);
         }
+
+       /*private void OnPinchStarted(object sender, PinchStartedGestureEventArgs e)
+        {
+            initialScale = atransform.ScaleX;
+        }
+
+        private void OnPinchDelta(object sender, PinchGestureEventArgs e)
+        {
+            atransform.ScaleX = initialScale * e.DistanceRatio;
+            atransform.ScaleY = initialScale * e.DistanceRatio;
+        }*/
 
         public class CWRUMapMode : RoadMode
         {
@@ -37,11 +50,6 @@ namespace CWRUtility
                 validLats = new Range<double>(41.499707, 41.515711);
                 // The longitude value range (From = left most longitude, To = right most longitude)
                 validLongs = new Range<double>(-81.615243, -81.598334);
-
-                // The latitude value range (From = bottom most latitude, To = top most latitude)
-                // validLats = new Range<double>(47.58346, 47.62877);
-                // The longitude value range (From = left most longitude, To = right most longitude)
-                // validLongs = new Range<double>(-122.36298, -122.28599);
             }
 
             protected override Range<double> GetZoomRange(GeoCoordinate center)
@@ -96,83 +104,6 @@ namespace CWRUtility
                 return isChanged;
             }
 
-        }
-
-        public class SeattleMapMode : AerialMode
-        {
-            private Range<double> validLatitudeRange;
-            private Range<double> validLongitudeRange;
-
-            public SeattleMapMode()
-            {
-                // The latitude value range (From = bottom most latitude, To = top most latitude)
-                validLatitudeRange = new Range<double>(47.58346, 47.62877);
-                // The longitude value range (From = left most longitude, To = right most longitude)
-                validLongitudeRange = new Range<double>(-122.36298, -122.28599);
-            }
-
-            // Restricts the map view.
-            protected override Range<double> GetZoomRange(GeoCoordinate center)
-            {
-                // The allowable zoom levels - 14 to 21.
-                return new Range<double>(14, 21);
-            }
-
-
-            // This method is called when the map view changes on Keyboard 
-            // and Navigation Bar events.
-            public override bool ConstrainView(GeoCoordinate center, ref double zoomLevel, ref double heading, ref double pitch)
-            {
-                bool isChanged = base.ConstrainView(center, ref zoomLevel, ref heading, ref pitch);
-
-                double newLatitude = center.Latitude;
-                double newLongitude = center.Longitude;
-
-                // If the map view is outside the valid longitude range,
-                // move the map back within range.
-                if (center.Longitude > validLongitudeRange.To)
-                {
-                    newLongitude = validLongitudeRange.To;
-                }
-                else if (center.Longitude < validLongitudeRange.From)
-                {
-                    newLongitude = validLongitudeRange.From;
-                }
-
-                // If the map view is outside the valid latitude range,
-                // move the map back within range.
-                if (center.Latitude > validLatitudeRange.To)
-                {
-                    newLatitude = validLatitudeRange.To;
-                }
-                else if (center.Latitude < validLatitudeRange.From)
-                {
-                    newLatitude = validLatitudeRange.From;
-                }
-
-                // The new map view location.
-                if (newLatitude != center.Latitude || newLongitude != center.Longitude)
-                {
-                    center.Latitude = newLatitude;
-                    center.Longitude = newLongitude;
-                    isChanged = true;
-                }
-
-                // The new zoom level.
-                Range<double> range = GetZoomRange(center);
-                if (zoomLevel > range.To)
-                {
-                    zoomLevel = range.To;
-                    isChanged = true;
-                }
-                else if (zoomLevel < range.From)
-                {
-                    zoomLevel = range.From;
-                    isChanged = true;
-                }
-
-                return isChanged;
-            }
         }
     }
 }
