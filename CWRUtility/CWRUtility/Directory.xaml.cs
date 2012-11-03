90c956a1735e370e0988e3b39ff9ca6748655bf2
William Vernon Ordiwat <wvo2@case.edu>
2012-11-03 17:35:39 -0400

    Parser and listBox updates
    The text file is now read and converted to a string. the string is
    converted to a character array. the character array is read and creates
    a list of resources. the list of resources is read and and 2 text boxes
    are made for each resource, 1 for the name and 1 for all of the other
    strings (this one has it's visibility set to collapsed)

--- CWRUtility/CWRUtility/Directory.xaml.cs
+++ CWRUtility/CWRUtility/Directory.xaml.cs
@@ -43,7 +43,7 @@ namespace CWRUtility
 
         public void readFromFile()
         {
-
+            List<Resource> ListResource = new List<Resource>();
             string thefile = "";
             var resource = System.Windows.Application.GetResourceStream(new Uri("Resources/CampusResources.txt", UriKind.RelativeOrAbsolute));
             //StreamReader read = new StreamReader("C:/Users/VVilliam/Documents/GitHub/EECS-393/CWRUtility/CWRUtility/ResourcesCampusResources.txt");
@@ -59,9 +59,78 @@ namespace CWRUtility
            / T1.Text = thefile;*/
             StreamReader SR = new StreamReader(resource.Stream);
             thefile = SR.ReadToEnd();
+            char [] thechar = thefile.ToCharArray();
+            Resource temp = new Resource ("", "", "", "");
+            String Adder = "";
+            
 
+            int WhereAreWe = 0; //marks "CONCERNS" (Name, Phone, Location, and Info) with 0, 1, 2, and 3 respecfully.
+            int Slasher = 0; //indicates if the last char read was a '\' so we know when a new line was made in the text file so we can update WhereAreWe
 
+            for (int i = 0; i < thefile.Length; i++)
+            {
+                char Un = thechar[i];
+                if (Un.Equals('\\'))
+                {
+                    Slasher = 1;
+                }
+                else
+                {
+                    if (Un.Equals('r') && Slasher == 1) //new lines are \r\n, so this runs if we are at the beginning of a new line statement
+                    {
+                        if (WhereAreWe == 0)//the line we just finished reading was the Name
+                        {
+                            temp.name = Adder;
+                            Adder = "";//reset adder to an empty string
+                            WhereAreWe = WhereAreWe + 1; //indicate that what we're reading is a new "CONCERN"
+                        }
+                        if (WhereAreWe == 1)//the line we just finished reading was the Phone#
+                        {
+                            temp.phone = Adder;
+                            Adder = "";//reset adder to an empty string
+                            WhereAreWe = WhereAreWe + 1; //indicate that what we're reading is a new "CONCERN"
+                        }
+                        if (WhereAreWe == 2)//the line we just finished reading was the Address
+                        {
+                            temp.location = Adder;
+                            Adder = "";//reset adder to an empty string
+                            WhereAreWe = WhereAreWe + 1; //indicate that what we're reading is a new "CONCERN"
+                        }
+                        if (WhereAreWe == 3)//the line we just finished reading was the Short Description, the last data feild for a resource
+                        {
+                            temp.info = Adder;
+                            Adder = "";//reset adder to an empty string
+                            ListResource.Add(temp);
+                            WhereAreWe = 0;
+                        }
+                        i = i + 2; //we know that '\n' comes after '\r' so we can move i forward 2 to read whatever comes after '\r'
+                        Slasher = 0; //reset our '\' 
+                    }
+                    else
+                    {
+                        if (Slasher == 1)
+                        {
+                            Adder.Insert(Adder.Length, "\\"); //insert the '\' we read last time but did nothing with
+                        }
+                        Adder.Insert(Adder.Length, Un.ToString()); //insert the character we are evaluating.
+                    }
 
+                }
+              
+            }
+            //at this point ListResource should be populated
+            //time to make the ListResource present itself with The name in a large font in it's own Text Box and with the other feild below in smaller fonts and text boxes
+            int pos = 0;
+            for (int x = 0; x < ListResource.Count; x++)
+            {
+                TextBlock Temp = new TextBlock();
+                Temp.DataContext = temp.name;
+                listBox.Resources.Add(pos, Temp);
+                pos++;
+                Temp.DataContext = temp.phone + "\n" + temp.location + "\n" + temp.info;
+                Temp.Visibility = System.Windows.Visibility.Collapsed;
+                listBox.Resources.Add(pos, Temp);
+            }
         }
 
 
@@ -69,14 +138,16 @@ namespace CWRUtility
     }
     public class Resource
     {
-        public Resource(string name, string phone, string info)
+        public Resource(string name, string phone, string location, string info)
         {
             this.name = name;
             this.phone = phone;
             this.info = info;
+            this.location = location;
         }
         public string name { get; set; }
         public string phone { get; set; }
         public string info { get; set; }
+        public string location { get; set; }
     }
 }
