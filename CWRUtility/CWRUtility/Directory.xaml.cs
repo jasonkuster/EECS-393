2322b0048aec3f50d3505836fdbacd1f9073391d
William Vernon Ordiwat <wvo2@case.edu>
2012-11-04 03:15:30 -0500

    Directory (Mostly) Functional
    Asthetic Changes are neccessary, as well as the expand /collapse
    functionality.

--- CWRUtility/CWRUtility/Directory.xaml.cs
+++ CWRUtility/CWRUtility/Directory.xaml.cs
@@ -64,78 +64,80 @@ namespace CWRUtility
             char [] thechar = thefile.ToCharArray();
             Resource temp = new Resource ("", "", "", "");
             String Adder = "";
-            
-
+            int pos = 0;
             int WhereAreWe = 0; //marks "CONCERNS" (Name, Phone, Location, and Info) with 0, 1, 2, and 3 respecfully.
-            int Slasher = 0; //indicates if the last char read was a '\' so we know when a new line was made in the text file so we can update WhereAreWe
+            //int Slasher = 0; //indicates if the last char read was a '\' so we know when a new line was made in the text file so we can update WhereAreWe
 
             for (int i = 0; i < thefile.Length; i++)
             {
                 char Un = thechar[i];
-                if (Un.Equals('\\'))
+                if (Un.Equals('\r')) //if we are at the end of the line
                 {
-                    Slasher = 1;
+                    i = i + 1;// we know the next line is a \n which we can skip over
+                    if (WhereAreWe == 0)//the line we just finished reading was the Name
+                    {
+                        temp.name = Adder;
+                        //Adder = "";//reset adder to an empty string
+                        WhereAreWe = WhereAreWe + 1; //indicate that what we're reading is a new "CONCERN"
+                    }
+                    else if (WhereAreWe == 1)//the line we just finished reading was the Phone#
+                    {
+                        temp.phone = Adder;
+                        //Adder = "";//reset adder to an empty string
+                        WhereAreWe = WhereAreWe + 1; //indicate that what we're reading is a new "CONCERN"
+                    }
+                    else if (WhereAreWe == 2)//the line we just finished reading was the Address
+                    {
+                        temp.location = Adder;
+                       // Adder = "";//reset adder to an empty string
+                        WhereAreWe = WhereAreWe + 1; //indicate that what we're reading is a new "CONCERN"
+                    }
+                     else if (WhereAreWe == 3)//the line we just finished reading was the Short Description, the last data feild for a resource
+                    {
+                        temp.info = Adder;
+                        //Adder = "";//reset adder to an empty string
+                        ListResource.Add(temp);
+                        //listBox.i.Add(temp.name, temp.name);
+                        //listBox.FontSize = 40;
+                        //using (listBox.FontSize = 30)
+                         TextBox robot = new TextBox();
+                         robot.FontSize = 30;
+                         robot.Text = temp.name;
+                         listBox.Items.Add(robot);
+                        //pos++;
+                        //listBox.Resources.Add(temp.phone + "\n" + temp.location + "\n" + temp.info, temp.phone + "\n" + temp.location + "\n" + temp.info);
+                         PhoneTextBox cop = new PhoneTextBox();
+                         cop.Text = temp.phone;
+                         listBox.Items.Add(cop);
+                        listBox.Items.Add(temp.location + "\n" + temp.info);
+                        WhereAreWe = 0;
+                    }
+                    Adder = "";
+
                 }
                 else
                 {
-                    if (Un.Equals('r') && Slasher == 1) //new lines are \r\n, so this runs if we are at the beginning of a new line statement
-                    {
-                        if (WhereAreWe == 0)//the line we just finished reading was the Name
-                        {
-                            temp.name = Adder;
-                            Adder = "";//reset adder to an empty string
-                            WhereAreWe = WhereAreWe + 1; //indicate that what we're reading is a new "CONCERN"
-                        }
-                        if (WhereAreWe == 1)//the line we just finished reading was the Phone#
-                        {
-                            temp.phone = Adder;
-                            Adder = "";//reset adder to an empty string
-                            WhereAreWe = WhereAreWe + 1; //indicate that what we're reading is a new "CONCERN"
-                        }
-                        if (WhereAreWe == 2)//the line we just finished reading was the Address
-                        {
-                            temp.location = Adder;
-                            Adder = "";//reset adder to an empty string
-                            WhereAreWe = WhereAreWe + 1; //indicate that what we're reading is a new "CONCERN"
-                        }
-                        if (WhereAreWe == 3)//the line we just finished reading was the Short Description, the last data feild for a resource
-                        {
-                            temp.info = Adder;
-                            Adder = "";//reset adder to an empty string
-                            ListResource.Add(temp);
-                            WhereAreWe = 0;
-                        }
-                        i = i + 2; //we know that '\n' comes after '\r' so we can move i forward 2 to read whatever comes after '\r'
-                        Slasher = 0; //reset our '\' 
-                    }
-                    else
-                    {
-                        if (Slasher == 1)
-                        {
-                            Adder.Insert(Adder.Length, "\\"); //insert the '\' we read last time but did nothing with
-                        }
-                        Adder.Insert(Adder.Length, Un.ToString()); //insert the character we are evaluating.
-                    }
-
+                    Adder = Adder + Un.ToString();
                 }
-              
             }
             //at this point ListResource should be populated
             //time to make the ListResource present itself with The name in a large font in it's own Text Box and with the other feild below in smaller fonts and text boxes
-            int pos = 0;
-            for (int x = 0; x < ListResource.Count; x++)
-            {
-                TextBlock Temp = new TextBlock();
-                Temp.DataContext = temp.name;
-                listBox.Resources.Add(pos, Temp);
-                pos++;
-                Temp.DataContext = temp.phone + "\n" + temp.location + "\n" + temp.info;
-                Temp.Visibility = System.Windows.Visibility.Collapsed;
-                listBox.Resources.Add(pos, Temp);
-            }
+            //int pos = 0;
+           // for (int x = 0; x < ListResource.Count; x++)
+           // {
+                //TextBlock Temp = new TextBlock();
+                //Temp.DataContext = temp.name;
+                //listBox.Resources.Add(pos, Temp);
+            //    listBox.Resources.Add(pos, temp.name);
+            //    pos++;
+                //Temp.DataContext = temp.phone + "\n" + temp.location + "\n" + temp.info;
+                //Temp.Visibility = System.Windows.Visibility.Collapsed;
+                //listBox.Resources.Add(pos, Temp);
+           //     listBox.Resources.Add(pos, temp.phone + "\n" + temp.location + "\n" + temp.info);
+           // }
         }
 
-
+        //int toofar = 700;
 
     }
     public class Resource
@@ -152,4 +154,5 @@ namespace CWRUtility
         public string info { get; set; }
         public string location { get; set; }
     }
+    
 }
