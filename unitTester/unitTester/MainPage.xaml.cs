9fd5c70ec74a19bb51eb578ba28191e4fcb48873
Stuart Long <srl51@case.edu>
2012-11-12 07:07:58 -0500

    Added some unit testing stuff

---
+++ unitTester/unitTester/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Silverlight.Testing;
+
+namespace unitTester
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var testPage = UnitTestSystem.CreateTestPage();
+            IMobileTestPage imobileTPage = testPage as IMobileTestPage;
+            BackKeyPress += (s, arg) =>
+            {
+                bool navigateBackSuccessfull = imobileTPage.NavigateBack();
+                arg.Cancel = navigateBackSuccessfull;
+            };
+
+            (Application.Current.RootVisual as PhoneApplicationFrame).Content = testPage;
+        }
+    }
+}
